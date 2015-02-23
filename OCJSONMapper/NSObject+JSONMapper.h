bf164177c8f740e80cfd870c431184f60e35e69f
Ignacio Calderon <ignacio@backbase.com>
2015-02-23 14:53:35 +0100

    Initial commit

---
+++ OCJSONMapper/NSObject+JSONMapper.h
@@ -0,0 +1,16 @@
+//
+//  NSObject+JSONMapper.h
+//  JSONMapper
+//
+//  Created by Ignacio Calderon on 23/02/15.
+//  Copyright (c) 2015 Ignacio Calderon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (JSONMapper)
+
++(instancetype) map:(id)jsonObject;
+-(NSString*) JSONString;
+
+@end
---
+++ OCJSONMapper/NSObject+JSONMapper.m
@@ -0,0 +1,150 @@
+//
+//  NSObject+JSONMapper.m
+//  JSONMapper
+//
+//  Created by Ignacio Calderon on 23/02/15.
+//  Copyright (c) 2015 Ignacio Calderon. All rights reserved.
+//
+
+#import "NSObject+JSONMapper.h"
+#import <objc/runtime.h>
+
+@interface Property : NSObject
+@property (strong, nonatomic) NSString* name;
+@property (strong, nonatomic) NSString* type;
+@property (strong, nonatomic) NSString* subtype;
+@end
+@implementation Property
+@end
+
+@implementation NSObject (JSONMapper)
+
++(instancetype) map:(id)jsonObject{
+    return [[[self class] alloc] mapTo:jsonObject];
+}
+
+-(instancetype) mapTo:(id)jsonObject{
+    if([jsonObject isKindOfClass:[NSDictionary class]])
+        return [self mapToDictionary:jsonObject];
+    if([jsonObject isKindOfClass:[NSArray class]])
+        return [self mapToArray:jsonObject];
+    return [self mapToValue:jsonObject];
+}
+
+-(instancetype) mapToDictionary:(NSDictionary*)data{
+    for(Property* property in [self getProperties]){
+        if(data[property.name] == nil) //property is not in the json, ignore it.
+            continue;
+        
+        id value = data[property.name];
+        if([value isKindOfClass:[NSDictionary class]]){
+            //map as an object
+            [self setValue:[NSClassFromString(property.type) map:value] forKey:property.name];
+        }else if([value isKindOfClass:[NSArray class]]){
+            //see how to create the instances of the corresponding type.
+            [self setValue:[NSClassFromString(property.subtype) map:value] forKey:property.name];
+        }else{
+            //assign the value to the property.
+            [self setValue:value forKey:property.name];
+        }
+    }
+    
+    return self;
+}
+
+-(instancetype) mapToArray:(NSArray*)data{
+    NSMutableArray* result = [NSMutableArray array];
+    for(id item in data){
+        [result addObject:[[self class] map:item]];
+    }
+    return result;
+}
+
+-(instancetype) mapToValue:(id)data{
+    if(data == nil) return [NSNull alloc];
+    return data;
+}
+
+-(NSArray*) getProperties{
+    NSMutableArray* properties = [NSMutableArray array];
+    
+    Class currentClass=[self class];
+    while (currentClass && currentClass != [NSObject class]) {
+        unsigned int propertyCount;
+        objc_property_t* list = class_copyPropertyList(currentClass, &propertyCount);
+        
+        for(int i=0;i<propertyCount;i++){
+            objc_property_t prop = list[i];
+            
+            const char* type = property_getAttributes(prop);
+            NSString* typeString = [NSString stringWithUTF8String:type];
+            NSArray* attributes = [typeString componentsSeparatedByString:@","];
+            NSString* typeAttribute = [attributes objectAtIndex:0];
+            NSString* subTypeString=nil;
+            
+            if ([typeAttribute hasPrefix:@"T@"]) {
+                typeString = [typeAttribute substringWithRange:NSMakeRange(3, [typeAttribute length]-4)];
+                Class typeClass = NSClassFromString(typeString);
+                if(typeClass == nil){
+                    NSArray* tokens = [typeString componentsSeparatedByCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"<>"]];
+
+                    typeString = tokens[0];
+                    subTypeString = tokens[1];
+                }
+            }
+            
+            Property* property = [[Property alloc] init];
+            property.name = [NSString stringWithCString:property_getName(prop) encoding:NSUTF8StringEncoding];
+            property.type = typeString;
+            property.subtype = subTypeString;
+            [properties addObject:property];
+        }
+        
+        free(list);
+        
+        //copy parent properties too.
+        currentClass = class_getSuperclass(currentClass);
+    }
+
+    return properties;
+}
+
+-(NSString*) JSONString {
+    if(self == nil || [self isKindOfClass:[NSNull class]])
+        return @"null";
+    
+    if([self isKindOfClass:[NSNumber class]]){
+        if([self isKindOfClass:[@(YES) class]])
+            return [((NSNumber*)self) boolValue] ? @"true" : @"false";
+
+        return [((NSNumber*)self) stringValue];
+    }
+    
+    if([self isKindOfClass:[NSString class]])
+        return [NSString stringWithFormat:@"\"%@\"",(NSString*)self];
+    
+    
+    if([self isKindOfClass:[NSArray class]]){
+        NSMutableString* buffer = [NSMutableString string];
+        [buffer appendString:@"["];
+        for(id item in (NSArray*)self){
+            if(buffer.length > 1) [buffer appendString:@","];
+            [buffer appendString:[item JSONString]];
+        }
+        [buffer appendString:@"]"];
+        return buffer;
+    }
+    
+    //else is an object
+    NSMutableString* buffer = [NSMutableString string];
+    [buffer appendString:@"{"];
+    for(Property* property in [self getProperties]){
+        if(buffer.length > 1) [buffer appendString:@","];
+        id value = [self valueForKey:property.name];
+        [buffer appendString:[NSString stringWithFormat:@"\"%@\": %@", property.name, [value JSONString]]];
+    }
+    [buffer appendString:@"}"];
+    return buffer;
+}
+
+@end
